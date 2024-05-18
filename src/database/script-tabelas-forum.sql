d7a094ef272b6fa439120fb5e87f3a7df1104805
João <joao.melo@sptech.school>
2024-05-18 17:37:26 -0300

    feat: Página home.html do fórum adicionada

--- src/database/script-tabelas-forum.sql
+++ src/database/script-tabelas-forum.sql
@@ -3,9 +3,52 @@ CREATE DATABASE voleiForum;
 USE voleiForum;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50)
+	senha VARCHAR(50),
+    fotoPerfil MEDIUMBLOB
 );
+
+CREATE TABLE postagem(
+	idPostagem INT PRIMARY KEY AUTO_INCREMENT,
+    assunto VARCHAR(45),
+    descricao VARCHAR(255),
+    dataHora DATETIME,
+    fkUsuario INT,
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+);
+
+CREATE TABLE comentario(
+	idComentario INT,
+    fkPostagem INT,
+    fkUsuario INT,
+    mensagem VARCHAR(255),
+    dataHora DATETIME,
+    FOREIGN KEY (fkPostagem) REFERENCES postagem(idPostagem),
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+    PRIMARY KEY (idComentario, fkPostagem, fkUsuario)
+); 
+
+CREATE TABLE curtida(
+	idCurtida INT,
+    fkPostagem INT,
+    fkUsuario INT,
+    FOREIGN KEY (fkPostagem) REFERENCES postagem(idPostagem),
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+    PRIMARY KEY (idCurtida, fkPostagem, fkUsuario)
+); 
+
+CREATE TABLE visualizacao(
+	idVisualizacao INT,
+    fkPostagem INT,
+    fkUsuario INT,
+    dataHora DATETIME,
+    FOREIGN KEY (fkPostagem) REFERENCES postagem(idPostagem),
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+    PRIMARY KEY (idVisualizacao, fkPostagem, fkUsuario)
+); 
+
+INSERT INTO usuario (nome, email, senha) VALUES ('João Victor', 'joao@gmail.com', 'joao@gmail.com1');
+
 SELECT * FROM usuario;
